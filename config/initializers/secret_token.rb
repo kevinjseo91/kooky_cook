63fcc2de1f0d0a93804e464160c39496d5e2188c
Kevin Seo <kevinjseo91@gmail.com>
2013-11-20 06:36:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KookyCook::Application.config.secret_key_base = '22ca21b435f46056a948dd968a6dca5d276614aea5277413533d8a95bdbaa1afb1975539354de8e0564d63abc6e716290a22fcbb186c7d1cc36181fb976f0465'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KookyCook::Application.config.session_store :cookie_store, key: '_kooky_cook_session'
